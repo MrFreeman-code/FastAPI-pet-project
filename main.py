d138297b4208f435fd6e1246c59a44924e9dc5ea
Didimus <didimus@mail>
2025-03-16 17:27:04 +0300

    Сделал редирект на /docs при запуске web-сервера

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 # asynccontextmanager — это декоратор из модуля contextlib в Python, который позволяет создавать асинхронные контекстные менеджеры.
 from contextlib import asynccontextmanager
@@ -19,7 +20,19 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import RedirectResponse
+
+
+class RedirectToDocsMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        if request.url.path == "/":
+            return RedirectResponse(url="/docs")
+        response = await call_next(request)
+        return response
+
+
+app.add_middleware(RedirectToDocsMiddleware)
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
