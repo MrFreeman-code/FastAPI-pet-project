dfa9c687254828d9fae4b7b22c8e24a7402a9c8c
Didimus <didimus@mail>
2025-03-16 17:37:31 +0300

    small commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 # asynccontextmanager — это декоратор из модуля contextlib в Python, который позволяет создавать асинхронные контекстные менеджеры.
+from router import router as tasks_router
 from contextlib import asynccontextmanager
 from db import create_tables, delete_tables
-from router import router as tasks_router
 
+from starlette.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 
 # Теперь при запуске web-сервера, сначала удаляются все таблицы, а затем создаются все таблицы через lifespan.
 @asynccontextmanager
@@ -20,10 +22,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import RedirectResponse
 
-
+#  Middleware в FastAPI (и Starlette) позволяет вам обрабатывать запросы и ответы, добавляя дополнительную логику, такую как аутентификация, логирование или, в данном случае, перенаправление.
 class RedirectToDocsMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         if request.url.path == "/":
@@ -31,8 +31,9 @@ class RedirectToDocsMiddleware(BaseHTTPMiddleware):
         response = await call_next(request)
         return response
 
-
+# Cтрока добавляет middleware RedirectToDocsMiddleware в приложение FastAPI, которое автоматически перенаправляет пользователей с корневого адреса на страницу документации FastAPI.
 app.add_middleware(RedirectToDocsMiddleware)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080, reload=True)
