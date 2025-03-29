6a42c666a12c6afe59224685f6cf27f37395d784
Didimus <didimus@mail>
2025-03-29 16:08:53 +0300

    Добавил авторизацию для других роутов

--- api/routers/task.py
+++ api/routers/task.py
@@ -4,7 +4,10 @@ from fastapi import APIRouter, Depends, status
 
 from typing import Annotated
 from core.interlayer import task as t
-from api.models.task import TaskAdd, TasksOut, TaskId
+from api.models.task import TaskAdd, TaskId, TasksOut
+
+from api.models.auth import User
+from core.interlayer.auth import get_current_active_user
 
 # prefix у всей группы роутов будет одинаковый, и в tags прописываем название группый роутов
 router = APIRouter(prefix="/tasks", tags=["Tasks"])
@@ -13,6 +16,7 @@ router = APIRouter(prefix="/tasks", tags=["Tasks"])
 # Добавляется async, тк работа с БД в асинхронном режиме и ф-ии тоже долны быть асинхронны
 @router.post("/add", summary="Создать таску")
 async def add_one_task(
+    current_user: Annotated[User, Depends(get_current_active_user)],
     task: Annotated[TaskAdd, Depends()]
 ) -> TaskId:
     task_id = await t.add_one_task(task)
@@ -20,6 +24,8 @@ async def add_one_task(
 
 
 @router.get("/get-all", summary="Получить все таски")
-async def get_all_tasks() -> list[TasksOut]:
+async def get_all_tasks(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+) -> list[TasksOut]:
     tasks = await t.get_all_tasks()
     return tasks
